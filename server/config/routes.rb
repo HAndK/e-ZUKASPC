6d34eceb0dfe19b51dc7ef39f9f6bc5753caf43e
Hiroyuki Inoue <syslink.h.inoue@gmail.com>
2014-09-01 10:31:36 +0900

    デザインを刷新

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/auth/twitter/callback', :to => 'sessions#create'
-  resources :tags, :only => [:index, :show]
+  resources :tags, :only => [:index, :show, :new, :create]
   mount Tag::API => '/tags'
+  
+  root :to => "tags#new"
 end
