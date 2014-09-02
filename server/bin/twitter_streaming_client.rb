d86a35c671a3e3f05303cec8d315ce215cf31739
tigawa <ihciiat@gmail.com>
2014-09-02 12:35:04 +0900

    twitter_streaming_clinetにデバッグ用のログを仕込んだ

--- server/bin/twitter_streaming_client.rb
+++ server/bin/twitter_streaming_client.rb
@@ -12,30 +12,37 @@ require File.expand_path("../lib/ext/twitter", File.dirname(__FILE__))
 $stdout.sync = true
 
 class TwitterStreamingClient
-  def initialize(host, port)
-    @client = Net::HTTP.new(host, port)
+  # def initialize(host, port)
+  def initialize
+    # @client = Net::HTTP.new(host, port)
     @twitter_client = Twitter::Streaming::Client.new do |config|
-      config.consumer_key = ENV["CONSUMER_KEY"]
-      config.consumer_secret = ENV["CONSUMER_SECRET"]
-      config.access_token = ENV["ACCESS_TOKEN"]
-      config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+      config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token = ENV["TWITTER_OAUTH_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_OAUTH_TOKEN_SECRET"]
+
+      p config
     end
   end
 
   def start
+    puts "started!!"
     @twitter_client.sample do |tweet|
+      puts "-> tweet!!"
       next if tweet.lang != "ja" && tweet.user.time_zone != "Tokyo"
       params = { tweet: {} }
       params[:tweet][:text] = tweet.text
       params[:tweet][:screen_name] = tweet.user.screen_name
       params[:tweet][:icon_url] = tweet.user.profile_image_url
-      @client.post("/streaming", params.to_query)
+      p params[:tweet]
+      # @client.post("/streaming", params.to_query)
     end
   end
 end
 
-if ARGV[0] == "start" && ARGV.length == 3
-  TwitterStreamingClient.new(ARGV[1], ARGV[2].to_i).start
-else
-  abort "Usage: twitter_streaming_client start <host> <port>"
-end
+# if ARGV[0] == "start" && ARGV.length == 3
+  TwitterStreamingClient.new().start
+  # TwitterStreamingClient.new(ARGV[1], ARGV[2].to_i).start
+# else
+#   abort "Usage: twitter_streaming_client start <host> <port>"
+# end
