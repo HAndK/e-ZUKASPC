0f578c4af8dd1250f851e3d5ccb0ec94a6b5eb91
tigawa <ihciiat@gmail.com>
2014-09-24 00:44:42 +0900

    tweetsテーブルに外部キー制約を追加

---
+++ server/db/migrate/20140916004848_create_tweets.rb
@@ -0,0 +1,12 @@
+class CreateTweets < ActiveRecord::Migration
+  def change
+    create_table :tweets do |t|
+    	t.references :user, null: false # ユーザテーブルへの外部キー
+      t.text :text, null: false #ツイート
+      t.string :hashtags, array: true, default: [] #ハッシュタグ
+      t.timestamps
+    end
+
+    add_foreign_key :tweets, :users
+  end
+end
