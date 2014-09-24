5d494c97c3e26bc0e4f3cde9b41f097162343931
tigawa <ihciiat@gmail.com>
2014-09-24 18:52:02 +0900

    tweetをscaffoldした直後

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :tweets
+
   get '/auth/twitter/callback', :to => 'sessions#create'
   resources :tags, :only => [:index, :show, :new, :create]
   mount Tag::API => '/tags'
