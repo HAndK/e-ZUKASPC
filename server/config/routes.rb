b1e7e42a74c26ccbbc2f2bec2405dd81ad74410b
tigawa <ihciiat@gmail.com>
2014-09-24 23:42:01 +0900

    .

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :tweets
 
   get '/auth/twitter/callback', :to => 'sessions#create'
   resources :tags, :only => [:index, :show, :new, :create]
   mount Tag::API => '/tags'
+  
+  resources :tweets, :only => [:index, :create]
   
   root :to => "tags#new"
 end
